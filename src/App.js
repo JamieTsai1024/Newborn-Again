7bb5b244a408672ed8b4d8a7b87bb8257f114d80
emilyhuxng <emilyjiajiahuang@gmail.com>
2023-03-04 22:05:10 -0500

    Make landing page and browse by category section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from "./logo.svg";
+import { createTheme, ThemeProvider } from "@mui/material";
 import "./App.css";
+import BrowseByCategory from "./pages/BrowseByCategory";
 import LandingPage from "./pages/LandingPage";
 import MissionStatement from "./pages/MissionStatement";
+import React from "react";
+
+const theme = createTheme({
+  typography: { fontFamily: "Nunito, sans-serif" },
+});
 
 function App() {
+  const scrollClickHandler = () => {
+    missionStatementRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const missionStatementRef = React.createRef();
+
   return (
-    <>
-      <LandingPage />
-      <MissionStatement />
-    </>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <LandingPage onScrollClick={scrollClickHandler} />
+        <MissionStatement ref={missionStatementRef} />
+        <BrowseByCategory />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/pages/MissionStatement.jsx
+++ src/pages/MissionStatement.jsx
@@ -1,26 +1,46 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
-const MissionStatement = () => {
+const MissionStatement = React.forwardRef((props, ref) => {
   return (
-    <div>
-      <Typography variant="h2">Our Mission</Typography>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare
-        tincidunt nunc et efficitur. Maecenas rhoncus, mi a faucibus mattis,
-        arcu arcu ornare dui, a cursus nulla nunc porta augue. Maecenas
-        scelerisque mauris eget odio elementum feugiat. Vivamus cursus ligula
-        arcu. Donec interdum pretium ante. Vivamus sollicitudin arcu at massa
-        varius, a volutpat ante tristique. Integer bibendum tristique orci
-        laoreet vehicula. Nunc quis porttitor lacus. Praesent luctus orci ipsum,
-        id vestibulum turpis rutrum varius. Nam pulvinar tristique viverra.
-        Etiam finibus massa eget erat gravida, vel imperdiet ipsum eleifend.
-        Curabitur dictum vitae ipsum ac lobortis. Cras et eleifend ante, id
-        pulvinar ipsum. Curabitur velit purus, consectetur vitae faucibus non,
-        maximus sit amet lorem.
-      </p>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        background: "#E8DFF5",
+        pb: "60px",
+      }}
+      ref={ref}
+    >
+      <Box
+        sx={{
+          width: "80%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h2" sx={{ py: "60px", fontWeight: 500 }}>
+          Our Mission
+        </Typography>
+        <Typography variant="h6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          ornare tincidunt nunc et efficitur. Maecenas rhoncus, mi a faucibus
+          mattis, arcu arcu ornare dui, a cursus nulla nunc porta augue.
+          Maecenas scelerisque mauris eget odio elementum feugiat. Vivamus
+          cursus ligula arcu. Donec interdum pretium ante. Vivamus sollicitudin
+          arcu at massa varius, a volutpat ante tristique. Integer bibendum
+          tristique orci laoreet vehicula. Nunc quis porttitor lacus. Praesent
+          luctus orci ipsum, id vestibulum turpis rutrum varius. Nam pulvinar
+          tristique viverra. Etiam finibus massa eget erat gravida, vel
+          imperdiet ipsum eleifend. Curabitur dictum vitae ipsum ac lobortis.
+          Cras et eleifend ante, id pulvinar ipsum. Curabitur velit purus,
+          consectetur vitae faucibus non, maximus sit amet lorem.
+        </Typography>
+      </Box>
+    </Box>
   );
-};
+});
 
 export default MissionStatement;
