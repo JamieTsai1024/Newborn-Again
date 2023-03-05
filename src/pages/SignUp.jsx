e583b8d303c176ca2d9955c854659fec4f4d15f3
Jamie1024 <jamie.tsai88@gmail.com>
2023-03-04 19:30:18 -0500

    add user sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,55 +1,71 @@
-import api from "../api/api";
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { account } from "../api/api";
+import { ID } from "appwrite";
+import { useNavigate } from "react-router";
 
 const SignUp = () => {
-  //   const handleSignup = async () => {
-  //     // e.preventDefault();
-  //     try {
-  //       register().then((account) =>
-  //         alert(`successfully created account with id: ${account.$id}`)
-  //       );
-  //       await api.createAccount(void {}, {
-  //         onSuccess: () => {
-  //           console.log("hi");
-  //         },
-  //         onError: () => {
-  //           console.log("bye");
-  //         },
-  //       });
-  //       await api.createSession("me@example.com", "password");
-  //       // dispatch({ type: FetchState.FETCH_SUCCESS, payload: user });
-  //     } catch (e) {
-  //       // dispatch({ type: FetchState.FETCH_FAILURE });
-  //     }
-  //   };
-  const handleSignup = async () => {
-    // e.preventDefault();
-    try {
-      await api.createAccount(void {}, {
-        onSuccess: () => {
-          console.log("hi");
-        },
-        onError: () => {
-          console.log("bye");
-        },
-      });
-      await api.createSession("me@example.com", "password");
-      // dispatch({ type: FetchState.FETCH_SUCCESS, payload: user });
-    } catch (e) {
-      // dispatch({ type: FetchState.FETCH_FAILURE });
-    }
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    account.create(ID.unique(), user.email, user.password, user.name).then(
+      (response) => {
+        console.log("response", response);
+        navigate("/");
+      },
+      (error) => {
+        console.log("error", error);
+      }
+    );
   };
+
   return (
-    <div>
-      <Button
-        variant="contained"
-        size="large"
-        sx={{ borderRadius: "16px", my: "20px" }}
-        onClick={handleSignup}
-      >
-        Create account
-      </Button>
-    </div>
+    <section className="container h-screen mx-auto flex">
+      <div className="flex-grow flex flex-col max-w-xl justify-center p-6">
+        <form onSubmit={handleSignUp}>
+          <label className="block mt-6">Name</label>
+          <input
+            className="w-full p-4 placeholder-gray-400 text-gray-700 bg-white text-lg border-0 border-b-2 border-gray-400 focus:ring-0 focus:border-gray-900"
+            type="text"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+            name="name"
+            autoComplete="name"
+          />
+
+          <label className="block mt-6">Email</label>
+          <input
+            className="w-full p-4 placeholder-gray-400 text-gray-700 bg-white text-lg border-0 border-b-2 border-gray-400 focus:ring-0 focus:border-gray-900"
+            type="text"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            name="email"
+            autoComplete="email"
+          />
+          <label className="block mt-6">Password</label>
+          <input
+            className="w-full p-4 placeholder-gray-400 text-gray-700 bg-white text-lg border-0 border-b-2 border-gray-400 focus:ring-0 focus:border-gray-900"
+            type="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            name="password"
+            autoComplete="password"
+          />
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              disabled={!user.name || !user.email || !user.password}
+              className="mx-auto mt-4 py-4 px-16 font-semibold rounded-lg shadow-md bg-gray-900 text-white border hover:border-gray-900 hover:text-gray-900 hover:bg-white focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
