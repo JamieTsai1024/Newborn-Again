bf22126914b468afdd007eec506c6790f737e440
Christina Zhang <christinaytzhangroxs@gmail.com>
2023-03-04 21:43:19 -0500

    add browse to page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import MissionStatement from "./pages/MissionStatement";
+import Browse from "./pages/Browse";
+
 
 function App() {
   return (
     <>
       <LandingPage />
       <MissionStatement />
+      <Browse/>
     </>
   );
 }
